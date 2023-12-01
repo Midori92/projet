bdfc2c75278f011de073a07cc437368fe45c22b0
elodie <elodie.mathurin@edu.ece.fr>
2023-12-01 22:15:45 +0100

    well et powell

--- main.c
+++ main.c
@@ -26,6 +26,7 @@ typedef struct graphe {
     arette* sommet;
     int ordre;
     int taille;
+    int* liste;
     int** adj;
 }graphe;
 
@@ -49,7 +50,7 @@ void tri(int* liste, int max) //tri des degre des sommet par ordre décroissant
 
 
 
-graphe* lecture(char* fichier){
+graphe* exclusion(char* fichier){
     {
 
         graphe * g=(graphe*)malloc(sizeof(graphe));
@@ -80,7 +81,7 @@ graphe* lecture(char* fichier){
         //retour au debut du texte
         fseek(ifs, 0, SEEK_SET);
 
-        int* liste = malloc (50*sizeof(int));
+        g-> liste = malloc (sizeof(int*));
         int nbs =0; // compteur nombre de sommet
 
        //lecture de chaque contraintes
@@ -89,40 +90,38 @@ graphe* lecture(char* fichier){
            int v2 = 0; //valeur ors du parcours de liste 1 si op2 deja là
            // créer les arêtes du graphe
            fscanf(ifs, "%d%d", &op1, &op2);
-           printf("ARET %d VS %d\n\n", op1, op2);
-           printf("sommet pour l'instant: %d\n", nbs);
+           //printf("ARET %d VS %d\n\n", op1, op2);
+           //printf("sommet pour l'instant: %d\n", nbs);
            g->sommet[i].op1.val = op1;
            g->sommet[i].op2.val = op2;
 
            if (nbs >= 1) {
                for (int j = 0; j < nbs; j++) {
-                   if (liste[j] == op1) //si op1 deja dans la liste
+                   if (g->liste[j] == op1) //si op1 deja dans la liste
                    {
                        v1 = 1;
-                       printf("op1 == %d deja dans liste \n", op1);
+
                    }
 
-                   if (liste[j] == op2) //si op2 deja dans la liste
+                   if (g->liste[j] == op2) //si op2 deja dans la liste
                    {
                        v2 = 1;
-                       printf("op2 == %d deja dans liste \n", op2);
                    }
-
                }
-
+/*
                    if (v1 == 1 && v2 == 1) {
-                       printf("les 2 sont deja dans liste: %d et %d  \n", op1, op2);
+                      /// printf("les 2 sont deja dans liste: %d et %d  \n", op1, op2);
                    }
-
+*/
                    if (v1 == 0) {
-                       liste[nbs] = op1;
-                       printf("ajout du sommet %d dans la liste\n", liste[nbs]);
+                       g->liste[nbs] = op1;
+                       //printf("ajout du sommet %d dans la liste\n", g->liste[nbs]);
                        nbs++;
                    }
 
                    if (v2 == 0) {
-                       liste[nbs] = op2;
-                       printf("ajout du sommet %d dans la liste \n", liste[nbs]);
+                       g->liste[nbs] = op2;
+                       //printf("ajout du sommet %d dans la liste \n", g->liste[nbs]);
                        nbs++;
                    }
 
@@ -130,24 +129,19 @@ graphe* lecture(char* fichier){
            }
 
            else { //nbs ==0
-                liste[nbs] = op1;
+               g->liste[nbs] = op1;
                 nbs++;
-                liste[nbs] = op2;
+               g->liste[nbs] = op2;
                 nbs++;
            }
        }
-       printf("\n\n NOMBRE SOMMET FINAL/ %d\n",nbs);
+      // printf("\n\n NOMBRE SOMMET FINAL/ %d\n",nbs);
 
        g->ordre = nbs;
-       g->adj = (int**) malloc(sizeof(int*)*g->ordre); //matrice d'adjacence
-
-        // initiatlisation de la matrice
-       for (int i = 0;i>g->ordre; i++){
-           for (int j = 0;j>g->ordre; j++){
-              g-> adj[i][j] = 0;
-           }
+       g->adj = (int**) calloc(g->ordre, sizeof(int *)); //matrice d'adjacence
+       for (int i =0; i< g->ordre; i++){
+           g->adj[i] = (int*) calloc(g->ordre, sizeof(int));
        }
-/////////////////////////////////
 
 
        int s; //sommet temporaire
@@ -155,7 +149,7 @@ graphe* lecture(char* fichier){
 
        for (int k=0; k < g->ordre; k++){
 
-           s = liste[k];
+           s = g->liste[k];
            int cpt = 0; //compte les degre sur sommet s
 
            for (int j = 0; j < g->taille; j++) {
@@ -174,81 +168,132 @@ graphe* lecture(char* fichier){
 
            }
 
-           printf("DEGRE DE %d: %d\n",s, cpt);
+          // printf("DEGRE DE %d: %d\n",s, cpt);
            deg[k] = cpt;
        }
 
-       printf("\n\nLISTE DEGRE 1: \n");
+     //  printf("\n\nLISTE DEGRE 1: \n");
 
        for(int i=0;i< g->ordre; i++){
-           printf("Sommet : %d degre: %d \n",liste[i],deg[i]);
+           //printf("Sommet : %d degre: %d \n",g->liste[i],deg[i]);
        }
 
         tri(deg,g->ordre);
 
-        printf("\n\nLISTE DEGRE 2: \n");
+      // printf("\n\nLISTE SOMMET RENG2\n");
+       for (int i =0; i <g->ordre; i++){
+           //printf("%d\n",g->liste[i]);
+       }
+
+
+        //printf("\n\nLISTE DEGRE 2: \n");
 ///sommet et indice
         for(int i=0;i< g->ordre; i++) {
-            printf("Sommet : %d degre: %d \n", liste[i], deg[i]);
+           // printf("Sommet : %d degre: %d \n", g->liste[i], deg[i]);
             for (int j = 0; j < g->ordre; j++) {
-                if(g->sommet[j].op1.val == liste[i]){
+                if(g->sommet[j].op1.val == g->liste[i]){
                     g->sommet[j].op1.indice = i;
-                    printf("1- %d d'indice %d\n",g->sommet[j].op1.val,g->sommet[j].op1.indice);
+                  //  printf("1- %d d'indice %d\n",g->sommet[j].op1.val,g->sommet[j].op1.indice);
                 }
 
-                if(g->sommet[j].op2.val == liste[i]){
+                if(g->sommet[j].op2.val == g->liste[i]){
                     g->sommet[j].op2.indice = i;
-                    printf("2- %d d'indice %d\n",g->sommet[j].op2.val,g->sommet[j].op2.indice);
+                   // printf("2- %d d'indice %d\n",g->sommet[j].op2.val,g->sommet[j].op2.indice);
                 }
             }
 
         }
-/*
-        int* color = malloc(sizeof(int) * g->ordre); //0 partout si pas de couleur
-        int couleur = 0;
-        */
+
 /////
 //remplir matrice
 
         int indice1;
         int indice2;
-        printf("TAILLE: %d", g->taille);
 
         for(int i=0;i<g->taille;i++){
-            printf("ok");
+
             indice1 = g->sommet[i].op1.indice;
             indice2 = g->sommet[i].op2.indice;
-            printf("In1: %d\n",indice1);
-            printf("In2: %d\n",indice2);
 
             g->adj[indice1][indice2] = 1;
             g->adj[indice2][indice1] = 1;
 
         }
 
-        for (int i =0; i<g->ordre; i++){
-            for (int j =0; j<g->ordre; j++){
-                printf("%d - ",g->adj[i][j]);
-            }
-            printf("\n");
-        }
-
 
         //well et powell
-/*
+
+        printf("\n\nWELSH ET POWELL");
         int s1;
+        int* color = (int*) calloc(g->ordre,sizeof(int*)); //0 partout si pas de couleur
+        int couleur = 0;
+
 
         for(int i = 0; i<g->ordre; i++) {
-            s1 = liste[i];
-            if(color[i] == 0){
-             color[i] = couleur++;
+            s1 = g->liste[i]; //sommet dans l'ordre
+
+            if (color[i] == 0) { //si non colorer
+                color[i] = couleur++;
+               // printf("\n\nCOULEU: %d et s: %d\n", couleur, s1);
+
+
+                for (int j = i; j < g->ordre; j++) { // coloration de sommet en fonction de lien avec s1 ou non
+
+                    if (g->adj[i][j] == 0 && color[j] == 0) { // pas de lien avec s1
+                        //printf("%d n'as pas de lien avec %d\n", s1, g->liste[j]);
+                        color[j] = couleur;
+
+                        for (int l = i; l < g->ordre; l++) {
+
+                            if (g->adj[j][l] == 1 && color[l] > 0) { // lien entre sommet de meme couleur
+                                color[j] = 0;  //changment de couleur
+                               // printf("erreur: couleur %d lien entre %d et %d\n", couleur, g->liste[j], g->liste[l]);
+                                //printf("couleur de %d: %d\n",g->liste[j], color[j]);
+                                break;
+                            }
+
+                           /* else {
+                                printf("ok pour %d et %d \n", g->liste[j], g->liste[l]);
+                            }
+                            */
+
+                        }
+                    }
+
+
+                    if (g->adj[i][j] == 1 || color[j] >0) {
+
+                       // printf("NOPE %d: couleur : %d\n", g->liste[j],color[j]);
+
+                    }
+                }
+
             }
-
-            for(int j = 0; j<g->ordre; j++){
-
-
+/*
+            else{
+                printf("\n deja colo");
             }
+*/
+/*
+        printf("\n\n");
+            for (int j=0; j<g->ordre;j++){
+                if (color[j] == couleur){
+                    printf("%d - ",g->liste[j]);
+                }
+
         }*/
+    }
+
+        printf("couleur %d",couleur);
+
+        for(int i =1; i<= couleur;i++){
+            printf("\ncoueleur %d: ",i);
+                for (int j=0; j<g->ordre;j++){
+                    if (color[j] == i){
+                        printf("%d - ",g->liste[j]);
+                    }
+                }
+            }
 
 
         return g;
@@ -256,104 +301,10 @@ graphe* lecture(char* fichier){
 }
 
 
-/*
-// Fonction pour lire les contraintes d'exclusion à partir d'un fichier
-Exclusion* lecture(int* nombreContraintes) {
-
-    char nom[50];
-    printf("Entrez le nom du fichier (avec le .txt): \n");
-    scanf("%s",nom);
-
-    FILE* ifs = fopen(nom, "r");
-    if (!ifs) {
-        printf( "Erreur ouverture du fichier\n");
-        exit(-1);
-    }
-
-    int tailleTableau = 20;
-
-    Exclusion* contraintes = malloc(tailleTableau * sizeof(Exclusion));
-
-    int nbContraintes = 0;
-
-    while (fscanf(ifs, "%d %d", &contraintes[nbContraintes].operation1, &contraintes[nbContraintes].operation2) == 2) {
-        nbContraintes++;
-
-        if (nbContraintes == tailleTableau) {
-            tailleTableau *= 2;
-            contraintes = realloc(contraintes, tailleTableau * sizeof(Exclusion));
-        }
-    }
-
-    fclose(ifs);
-
-    printf("Liste des contraintes:\n");
-
-    for(int i =0; i<nbContraintes;i++)
-    {
-        printf("%d %d \n",contraintes[i].operation1, contraintes[i].operation2);
-    }
-
-    *nombreContraintes = nbContraintes;
-    return contraintes;
-}
-*/
-
-
-// Fonction pour vérifier si une paire d'opérations est autorisée sur une station
-int estAutoriseeSurStation(int station, int* affectations, Exclusion* contraintes, int nombreContraintes, int operation) {
-    for (int i = 0; i < nombreContraintes; i++) {
-        if ((contraintes[i].operation1 == operation || contraintes[i].operation2 == operation) &&
-            affectations[contraintes[i].operation1 - 1] == station && affectations[contraintes[i].operation2 - 1] == station) {
-            return 0; // La paire d'opérations interdites est affectée à la même station
-        }
-    }
-    return 1; // La paire d'opérations est autorisée sur la station
-}
-
-// Fonction pour répartir les opérations sur les stations en respectant les contraintes
-void repartirOperations(int nombreOperations, int nombreStations, int nombreContraintes, Exclusion* contraintes) {
-
-    int* affectations = calloc(nombreOperations, sizeof(int));
-
-    for (int i = 0; i < nombreOperations; i++) {
-        for (int j = 1; j <= nombreStations; j++) {
-            if (estAutoriseeSurStation(j, affectations, contraintes, nombreContraintes, i + 1)==1) {
-                affectations[i] = j;
-                break;
-            }
-        }
-    }
-
-    // Affichage des affectations
-    printf("\nRepartition des operations:\n");
-    for (int i = 0; i < nombreOperations; i++) {
-        printf("Operation %d affectee a la station %d\n", i + 1, affectations[i]);
-    }
-
-    free(affectations);
-}
-
-
-//graphe
-// nombre chromatique
-
-
 
 int main() {
 
-   /* int nombreContraintes;
-    Exclusion* contraintes = lecture( &nombreContraintes);
-
-    int nombreOperations = 35;  // Nombre total d'opérations
-    int nombreStations = 20;    // Nombre total de stations
-
-    repartirOperations(nombreOperations, nombreStations, nombreContraintes, contraintes);
-
-    // Libération de la mémoire
-    free(contraintes);
-    */
-   lecture("test.txt");
+   exclusion("test.txt");
 
     return EXIT_SUCCESS;
 }
